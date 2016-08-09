9d9ea8fe6a2a13548d34a853553769c0dd551af5
Dennis Breen <dennis.breen@nform.com>
2016-08-09 06:38:17 -0600

    News category update

--- public/js/app/database/tagFilters.ts
+++ public/js/app/database/tagFilters.ts
@@ -171,19 +171,34 @@ module App {
         "Tag" : "Around the District",
       },
       "Filter2" : {
-        "Tag" : "School Operations",
+        "Tag" : "Budget & Finance",
       },
       "Filter3" : {
-        "Tag" : "Employee Essentials",
+        "Tag" : "Centre for Education",
       },
       "Filter4" : {
-        "Tag" : "Superintendent",
+        "Tag" : "Curriculum & Assessment",
       },
       "Filter5" : {
-        "Tag" : "Student Management",
+        "Tag" : "Employee Essentials",
       },
       "Filter6" : {
-        "Tag" : "Curriculum & Assessment",
+        "Tag" : "Health & Safety",
+      },
+      "Filter7" : {
+        "Tag" : "Leadership",
+      },
+      "Filter8" : {
+        "Tag" : "Professional Development",
+      },
+      "Filter9" : {
+        "Tag" : "School Operations",
+      },
+      "Filter10" : {
+        "Tag" : "Student Management",
+      },
+      "Filter11" : {
+        "Tag" : "Technology",
       }
     }
 
