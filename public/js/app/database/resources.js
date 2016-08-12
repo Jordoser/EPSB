3321c93901f999f5b969a323018f9364660e7e2c
Dennis Breen <dennis.breen@nform.com>
2016-08-12 11:01:47 -0600

    Math Assessment resources

--- public/js/app/database/resources.js
+++ public/js/app/database/resources.js
@@ -4,46 +4,85 @@ var App;
         function ResourcesDatabase() {
             this.Resources = {
                 "Resource1": {
-                    "Id": "Math 30 Assessment",
-                    "Name": "Math 30 Assessment",
-                    "Description": "Assesment guide for Math 30 course",
-                    "MetadataId": "Math 30 Assessment Metadata",
-                    "Tags": ["10-12", "Mathematics", "Assessments"]
+                    "Id": "General Performance Rubric for Mathematics",
+                    "Name": "General Performance Rubric for Mathematics",
+                    "Description": "The criteria in the general rubric were selected to align with process skills outlined\
+           in the revised Program of Studies for Mathematics.",
+                    "MetadataId": "General Performance Rubric for Mathematics Metadata",
+                    "Tags": ["1-3", "4-6", "7-9", "10-12", "Mathematics", "Assessments"]
                 },
                 "Resource2": {
-                    "Id": "Math 9 Classroom Resource",
-                    "Name": "Math 9 Classroom Resource",
-                    "Description": "Resource for in class use regarding the course: Math 9",
-                    "MetadataId": "Math 9 Classroom Resource Metadata",
-                    "Tags": ["7-9", "Mathematics", "Classroom Resources"]
+                    "Id": "Omar’s Balance Beam – Exemplar",
+                    "Name": "Omar’s Balance Beam – Exemplar",
+                    "Description": "GR 7 – Specific Outcome: Demonstrate an understanding of preservation of equality by:\
+           1) modelling preservation of equality, concretely, pictorially and symbolically; 2) applying\
+            preservation of equality to solve equations.",
+                    "MetadataId": "Omar’s Balance Beam – Exemplar Metadata",
+                    "Tags": ["7-9", "Mathematics", "Assessments"]
                 },
                 "Resource3": {
-                    "Id": "Science 20 Research & Support",
-                    "Name": "Science 20 Research & Support",
-                    "Description": "Research project for Science 20 Curriculum",
-                    "MetadataId": "Science 20 Research & Support Metadata",
-                    "Tags": ["10-12", "Science", 'Research & Support']
+                    "Id": "Shopping with Allie – Exemplar",
+                    "Name": "Shopping with Allie – Exemplar",
+                    "Description": "GR 7 – Specific Outcome: Solve problems involving percents from 1% to 100%.",
+                    "MetadataId": "Shopping with Allie – Exemplar Metadata",
+                    "Tags": ["7-9", "Mathematics", "Assessments"]
                 },
                 "Resource4": {
-                    "Id": "Science 4 Assessment",
-                    "Name": "Science 4 Assessment",
-                    "Description": "Assesment guide for Science 4 course",
-                    "MetadataId": "Science 4 Assessment Metadata",
-                    "Tags": ["4-6", "Science", "Assessments"]
+                    "Id": "What's the mass of the container? – Exemplar",
+                    "Name": "What's the mass of the container? – Exemplar",
+                    "Description": "GR 7 – Specific Outcome: Create a table of values from a linear relation,\
+           graph the table of values, and analyze the graph to draw conclusions and solve problems.",
+                    "MetadataId": "What's the mass of the container? – Exemplar Metadata",
+                    "Tags": ["7-9", "Mathematics", "Assessments"]
                 },
                 "Resource5": {
-                    "Id": "Math and Science Grade 10 Focus on Reading",
-                    "Name": "Math and Science Grade 10 Focus on Reading",
-                    "Description": "This is a Math and Science focus on reading topic for grade 10 students",
-                    "MetadataId": "Math and Science Grade 10 Focus on Reading Metadata",
-                    "Tags": ["10-12", "Science", "Mathematics", "Focus on Reading"]
+                    "Id": "Have you lost your marbles? – Exemplar",
+                    "Name": "Have you lost your marbles? – Exemplar",
+                    "Description": "GR 8 – Specific Outcome: Graph and analyze two-variable linear relations.",
+                    "MetadataId": "Have you lost your marbles? – Exemplar Metadata",
+                    "Tags": ["7-9", "Mathematics", "Assessments"]
                 },
                 "Resource6": {
-                    "Id": "4th Grade Math and Science Research & Support",
-                    "Name": "4th Grade Math and Science Research & Support",
-                    "Description": "Research topic about math and science for fourth grade",
-                    "MetadataId": "4th Grade Math and Science Research & Support Metadata",
-                    "Tags": ["4-6", "Science", "Mathematics", "Research & Support"]
+                    "Id": "Helping Harold – Exemplar",
+                    "Name": "Helping Harold – Exemplar",
+                    "Description": "GR 8 – Specific Outcome: Demonstrate an understanding of percents greater\
+           than or equal to 0%, including greater than 100%.",
+                    "MetadataId": "Helping Harold – Exemplar Metadata",
+                    "Tags": ["7-9", "Mathematics", "Assessments"]
+                },
+                "Resource7": {
+                    "Id": "Claim the Prize – Exemplar",
+                    "Name": "Claim the Prize – Exemplar",
+                    "Description": "GR 9 – Specific Outcome: Explain and apply the order of operations, \
+          including exponents, with and without technology.",
+                    "MetadataId": "Claim the Prize – Exemplar Metadata",
+                    "Tags": ["7-9", "Mathematics", "Assessments"]
+                },
+                "Resource8": {
+                    "Id": "Dollars and Sense – Exemplar",
+                    "Name": "Dollars and Sense – Exemplar",
+                    "Description": "GR 9 – Specific Outcomes: 1) Generalize a pattern arising from a problem-solving\
+           context, using a linear equation, and verify by substitution. 2) Graph a linear relation, analyze\
+            the graph, and interpolate or extrapolate to solve problems.",
+                    "MetadataId": "Dollars and Sense – Exemplar Metadata",
+                    "Tags": ["7-9", "Mathematics", "Assessments"]
+                },
+                "Resource9": {
+                    "Id": "Quest A+",
+                    "Name": "Quest A+",
+                    "Description": "Quest A+ is Alberta Education's digital testing solution. \
+          This application contains released tests for Grade 9 PATs.",
+                    "MetadataId": "Quest A+ Metadata",
+                    "Tags": ["7-9", "Mathematics", "Assessments"]
+                },
+                "Resource10": {
+                    "Id": "Alberta Assessment Consortium Performance Assessments",
+                    "Name": "Alberta Assessment Consortium Performance Assessments",
+                    "Description": "This is a searchable database of authentic performance assessments\
+           including tasks for Grades 1-3.  These tasks are based on Alberta curriculum and \
+           include student materials, outcomes & criteria, competency connections, and student exemplars.",
+                    "MetadataId": "Alberta Assessment Consortium Performance Assessments Metadata",
+                    "Tags": ["1-3", "4-6", "7-9", "10-12", "Mathematics", "Assessments"]
                 }
             };
         }
